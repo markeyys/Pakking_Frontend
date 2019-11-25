69005ecce151271808bd821148ec7474a857f57f
markpang <1800591@sit.singaporetech.edu.sg>
2019-11-25 15:25:36 +0800

    Added analytics page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Register from './pages/Register';
 import Profile from './pages/Profile';
 import PrivateRoute from './pages/PrivateRoute';
 import { getProfile } from './store/actions/user';
+import Analytics from './pages/Analytics';
 
 class App extends Component {
   constructor(props) {
@@ -35,7 +36,7 @@ class App extends Component {
             <PrivateRoute path="/map" component={Home} />
             <PrivateRoute path="/about" component={About} />
             <PrivateRoute path="/profile" component={Profile} />
-
+            <PrivateRoute path="/graph" component={Analytics} />
           </Switch>
         </div>
       )
---
+++ src/pages/Analytics.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from './Main';
+
+const Analytics = () => (
+  <Main>
+    <h1>About Page</h1>
+  </Main>
+)
+
+export default Analytics;
